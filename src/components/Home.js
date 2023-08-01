064313777c3e0948798b5580f1baaac00453b712
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-01 15:12:30 +0100

    Finished Click Events

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 const Home = () => {
 
-    return (
-        <div className="">
+    const handleClick = () => {
+        console.log("HEllo")
+    }
 
-            <h3>Home</h3>
+    const handleClickAgain = (name, e) => {
+        console.log("hello "+name, e.target);
+    }
+
+    return (
+        <div className="home">
+            <h3>Homepage</h3>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('myName', e)}>Click again</button>
         </div>
     );
 
