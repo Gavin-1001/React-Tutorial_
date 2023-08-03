af654a92a406615c920959876e8b6831bb909b27
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-03 14:12:45 +0100

    Updated commit

---
+++ src/components/BlogList.js
@@ -0,0 +1,24 @@
+const BlogList = ({blogs, title}) => {
+
+    // const blogs = props.blogs;
+    // const title = props.title;
+    //console.log(props, blogs)
+
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) => ( //singular blog refers to each item in blogs array
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <p>{blog.body}</p>
+                </div>
+            ))}
+        </div>
+
+    )
+
+
+}
+
+export default BlogList;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import BlogList from "./BlogList";
 
 
 
@@ -13,13 +14,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs.map((blog) => ( //singular blog refers to each item in blogs array
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <p>{blog.body}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs"/>
         </div>
     );
 
