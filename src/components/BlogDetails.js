9390f51e5d72e811987b1d7dab0902a3196d1656
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 16:16:02 +0100

    Added a handleClick funciton that deletes a blog, added a onClick on the button in the article

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,11 +1,21 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import useFetch from "../customHooks/useFetch";
 
 const BlogDetails = () => {
 
     const {id} = useParams(); //allows us to grab route parameters from the route/url. In this case the :id
     const {data: blog, error, isLoading} = useFetch('http://localhost:8000/blogs/'+id); //using the useFetch Hook to get these three properties from useFetch
+    const navigate = useNavigate();
 
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs'+blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log("BLOG DELETED")
+            navigate('/')
+        })
+
+    }
 
     return (
         <div className="blog-details">
@@ -17,6 +27,7 @@ const BlogDetails = () => {
                     <h3>{blog.title}</h3>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>Delete blog</button>
                 </article>
             )}
         </div>
