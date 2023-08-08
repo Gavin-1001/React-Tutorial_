794400ab332824d30d58d3748490238fa4c24594
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 14:33:45 +0100

    Finished Tutorial 23_Router Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
+import {Link} from "react-router-dom";
+
 const Navbar = () => {
 
     return(
         <nav className="navbar">
             <h3>The Dojo Blog</h3>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" className="">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" className="">New Blog</Link>
             </div>
         </nav>
     )
