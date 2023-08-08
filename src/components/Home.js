b36fd79849c5fdd7fde8d60386620ec6e884e96a
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 12:04:17 +0100

    Finished Conditional Loading on loading for data coming back

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,32 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     // const handleDelete = (id) =>{
     //     const newBlogs = blogs.filter(blog => blog.id !== id);
     //     setBlogs(newBlogs);
     // }
     useEffect(() => {
+        setTimeout(() => { //the seTimeout simulates the isLoading conditional
         fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
+                setIsLoading(false);
             })
+        }, 1000);
     }, [])
+
 
     return (
         <div className="home">
             {/*{blogs && <BlogList blogs={blogs} />}*/}
-            {blogs &&<BlogList blogs={blogs} title="All Blogs!"  /> }
+            {isLoading && <div>Loading...</div>}
+            {blogs &&<BlogList blogs={blogs} title="All Blogs!"  /> } {/*Conditional template*/}
         </div>
     )
 }
