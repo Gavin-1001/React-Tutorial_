c4ad345c7c62f00dabb8c6a77e76c89c04b948d9
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 15:32:36 +0100

    Implemented useFetch hook into the BlogDetails, added styles to index

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,12 +1,24 @@
 import {useParams} from "react-router-dom";
+import useFetch from "../customHooks/useFetch";
 
 const BlogDetails = () => {
 
     const {id} = useParams(); //allows us to grab route parameters from the route/url. In this case the :id
+    const {data: blog, error, isLoading} = useFetch('http://localhost:8000/blogs/'+id); //using the useFetch Hook to get these three properties from useFetch
+
 
     return (
         <div className="blog-details">
-            <h3>Blog Details - {id}</h3>
+            {isLoading && <div>Still Loading....</div>}
+            {error && <div>{error}</div>}
+
+            {blog && (
+                <article>
+                    <h3>{blog.title}</h3>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     )
 
