e22a761001541a9d4dee1e75fabeeb982c59bfd8
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 16:05:17 +0100

    Added the content for the POST request to the form. Added a loading button

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,12 +5,21 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario'); //for select there has to be an initial value. The initial value will be shown when the form is loaded for the first time
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setIsLoading(true)
         const blog = {title, body, author}; //blog object
         console.log(blog)
-
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog) //turn json object into a string
+        }).then(() => {
+            console.log("New blog added.");
+            setIsLoading(false);
+        })
     }
 
     return(
@@ -34,7 +43,8 @@ const Create = () => {
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
-                <button>Add blog</button>
+                {!isLoading && <button>Add blog</button>}
+                {isLoading && <button disabled>Add blog...</button>}
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
