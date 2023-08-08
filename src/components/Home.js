fb738694b5a7cc0d4eeb528668987b466f29bf4a
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 12:22:44 +0100

    Finished Error Handling for the fetch request

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,28 +4,35 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    // const handleDelete = (id) =>{
-    //     const newBlogs = blogs.filter(blog => blog.id !== id);
-    //     setBlogs(newBlogs);
-    // }
     useEffect(() => {
         setTimeout(() => { //the seTimeout simulates the isLoading conditional
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                console.log(res)
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource :(')
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsLoading(false);
-            })
-        }, 1000);
+                setError(null);
+            }).catch(err => {
+                setIsLoading(false);
+                setError(err.message);
+
+        })
+        }, 500); //can add a timeout
     }, [])
 
 
     return (
         <div className="home">
             {/*{blogs && <BlogList blogs={blogs} />}*/}
+            {error && <div>{error} </div>}
             {isLoading && <div>Loading...</div>}
             {blogs &&<BlogList blogs={blogs} title="All Blogs!"  /> } {/*Conditional template*/}
         </div>
