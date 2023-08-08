750c51cbbe8e7182302ec41125a46d41a2ba90fc
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 15:52:04 +0100

    Added form to Create component, added styles in index

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,37 @@
+import {useState} from "react";
+
 const Create = () => {
 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario'); //for select there has to be an initial value. The initial value will be shown when the form is loaded for the first time
 
     return(
         <div className="create">
             <h2>Add a new blog!</h2>
+            <form>
+                <label>Blog Title: </label>
+                <input
+                    type="text"
+                    required
+                    onChange={(e) => setTitle(e.target.value)}
+                    />
+                <label>Blog body: </label>
+                <textarea
+                    required
+                    onChange={(e) => setBody(e.target.value)}
+                />
+
+                <label>Blog Author: </label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     )
 }
