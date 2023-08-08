113db3fa9d2e9f3a874e699f1ea460caf6bec47a
Gavin Shelley <gavinshelley10@gmail.com>
2023-08-08 11:58:38 +0100

    Finished Tutorial 17

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,27 +1,20 @@
-const BlogList = ({blogs, title, handleDeleteBlog}) => {
+const BlogList = ({blogs, title}) => {
 
     // const blogs = props.blogs;
     // const title = props.title;
     //console.log(props, blogs)
 
-
-
     return (
         <div className="blog-list">
-            <h2>{title}</h2>
-            {blogs.map((blog) => ( //singular blog refers to each item in blogs array
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <p>{blog.body}</p>
-                    <button onClick={() => handleDeleteBlog(blog.id)}>Delete blog</button>
+            <h3>{title}</h3>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id} >
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
                 </div>
             ))}
         </div>
-
-    )
-
-
+    );
 }
 
 export default BlogList;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,31 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
-
-
 const Home = () => {
+    const [blogs, setBlogs] = useState(null)
 
-    const [name, setName] = useState('mario');
-
-    const [blogs, setBlogs] = useState([
-        {title: "My new website", body: 'lorem ipsum.....', author: 'Mario', id: '1'},
-        {title: "My new website1", body: 'lorem ipsum.....', author: 'Mario', id: '2'},
-        {title: "My new website2", body: 'lorem ipsum.....', author: 'us', id: '3'},
-    ]);
-
-    const handleDeleteBlog = (id) => {
-        //newBlogs filters the current array of blogs and when the button is pressed it will iterate through the array of blogs
-        //id of blog that was to be deleted will not be filtered into the newBlogs variable, then you set the newBlogs as the setBlogs
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-
-        setBlogs(newBlogs);
-
-    }
-
+    // const handleDelete = (id) =>{
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
     useEffect(() => {
-        //the useEffect function ()=> will run everytime there is a rerender on the webpage,
-        //once initially when the component loads, and then anytime after when the data changes.
-        // best used for fetching data
-        console.log("use effect ran");
-        console.log(name)
-    }, [name]); //the useEffect will watch the name and only update when the name state has been updated, it is tied to the onclick button below
-
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDeleteBlog={handleDeleteBlog}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="All Blogs"/>
-            <button onClick={() => setName('Luigi')}>Change name</button>
-            <p>{name}</p>
-             {/*Only want to filter Mario's blogs*/}
+            {/*{blogs && <BlogList blogs={blogs} />}*/}
+            {blogs &&<BlogList blogs={blogs} title="All Blogs!"  /> }
         </div>
-    );
-
+    )
 }
+
+//npx json-server --watch data/db.json --port 8000
+
 export default Home;
